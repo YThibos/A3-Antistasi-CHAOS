1746acf37561d2fe027da039e5e58efe33c82d4b
YThibos <yannick.thibos@gmail.com>
2026-08-18 18:05:29 +0200

    Rebrand: CHAOS Antistasi branding with Chaos Group logos and upstream credit

--- A3A/mod_dev.cpp
+++ A3A/mod_dev.cpp
@@ -1,20 +1,20 @@
 #if __has_include("\x\A3A\addons\core\script_component.hpp")
 #include "\x\A3A\addons\core\script_component.hpp"
-name = QUOTE([Dev] Antistasi - Community VERSION);
-logo = QPATHTOFOLDER(Pictures\antistasi_logo_sw.paa);
-logoOver = QPATHTOFOLDER(Pictures\antistasi_logo.paa);
-logoSmall = QPATHTOFOLDER(Pictures\antistasi_logo_small.paa);
+name = QUOTE([Dev] CHAOS Antistasi VERSION);
+logo = QPATHTOFOLDER(Pictures\chaos_logo_sw.paa);
+logoOver = QPATHTOFOLDER(Pictures\chaos_logo.paa);
+logoSmall = QPATHTOFOLDER(Pictures\chaos_logo_small.paa);
 picture = QPATHTOFOLDER(Pictures\antistasi_overview.paa);
 #else
-name = "[Dev] Antistasi - Community version";
+name = "[Dev] CHAOS Antistasi";
 #endif
-author = "Antistasi Dev Team";
-description = "";
-tooltip = "[Dev] Antistasi";
-tooltipOwned = "Antistasi";
-overview = "<br/><t align='center' size='0.75'>Visit Antistasi on our <t href='https://antistasi.de/'>Website</t> | <t href='https://discord.gg/xZ5BqG9jWU'>Discord</t> | <t href='https://twitter.com/antistasio'>Twitter</t></t><br/><br/>";
+author = "Chaos Group";
+description = "Private fork of Antistasi Community Edition — custom loadouts, tiered base building and BAR integration.";
+tooltip = "[Dev] CHAOS Antistasi";
+tooltipOwned = "CHAOS Antistasi";
+overview = "<br/><t align='center' size='1.2' color='#d4af37'>[Dev] CHAOS Antistasi</t><br/><br/><t align='center' size='0.75'>A private tactical fork of Antistasi Community Edition,</t><br/><t align='center' size='0.75'>maintained by the Chaos Group.</t><br/><br/><t align='center' size='0.7'><t href='https://github.com/YThibos/A3-Antistasi-CHAOS'>github.com/YThibos/A3-Antistasi-CHAOS</t></t><br/><br/><t align='center' size='0.7' color='#ff9944'>Development build — not for public distribution.</t><br/><br/><t align='center' size='0.7' color='#aaaaaa'>Built on the outstanding foundation created by</t><br/><t align='center' size='0.7' color='#aaaaaa'>the Antistasi Community Dev Team and all contributors.</t><br/><t align='center' size='0.7' color='#aaaaaa'>Full credit and heartfelt gratitude to everyone at</t><br/><t align='center' size='0.7'><t href='https://antistasi.de/' color='#88bbff'>antistasi.de</t>  |  <t href='https://github.com/official-antistasi-community/A3-Antistasi' color='#88bbff'>upstream GitHub</t>  |  <t href='https://discord.gg/xZ5BqG9jWU' color='#88bbff'>Discord</t></t><br/>";
 actionName = "GitHub";
-action = "https://github.com/official-antistasi-community/A3-Antistasi";
+action = "https://github.com/YThibos/A3-Antistasi-CHAOS";
 hideName = 0;
 hidePicture = 0;
-dlcColor[] = {0.1765, 0.6392, 0.6235, 1};
+dlcColor[] = {0.83, 0.68, 0.21, 1};
